69ee808e6b6172e52dd7aee3bf32d12f0d558835
kazuki1023 <k.keio12569@gmail.com>
2023-09-04 12:58:33 +0900

    add patuchのルーティング

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\V1\TaskController;
+use App\Http\Controllers\Api\V1\CompleteTaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::prefix('/v1')->group(function () {
     Route::apiResource('/tasks', TaskController::class);
+    Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 });
